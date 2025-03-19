c8150131e91e28267945d9e42a53ef6e7ed86e25
lucasfagundesgweb <lucasfagundes.gweb@gmail.com>
2025-03-19 13:45:24 -0300

    feat: multiple muzzles added to the weapon behaviour.

--- Artists_Tavern_Project/Assets/Scripts/WeaponBehaviour.cs
+++ Artists_Tavern_Project/Assets/Scripts/WeaponBehaviour.cs
@@ -4,15 +4,29 @@ using UnityEngine.Events;
 public abstract class WeaponBehaviour : MonoBehaviour
 {
     [SerializeField] Rigidbody2D _prefab = null;
-    [SerializeField] Transform _muzzle = null;
+    [SerializeField] Transform _weaponRoot = null;
+    [SerializeField] Transform[] _muzzles = null;
     [SerializeField] float _shootForce = 10f;
 
     public event UnityAction<WeaponBehaviour> OnShoot = null;
 
     public virtual void Shoot()
     {
-        var _projectile = Instantiate(_prefab, _muzzle.position, _muzzle.rotation);
-        _projectile.linearVelocity = _muzzle.up.normalized * _shootForce;
-        OnShoot?.Invoke(this);
+        int _count = _muzzles.Length;
+
+        for (int i = 0; i < _count; i++)
+        {
+            var _muzzle = _muzzles[i];
+            var _projectile = Instantiate(_prefab, _muzzle.position, _muzzle.rotation);
+            _projectile.linearVelocity = _muzzle.up.normalized * _shootForce;
+            OnShoot?.Invoke(this);
+        }
+
+    }
+
+    public virtual void Aim(Transform _target)
+    {
+        var _direction = _target.position - transform.position;
+        _weaponRoot.up = _direction.normalized;
     }
 }
