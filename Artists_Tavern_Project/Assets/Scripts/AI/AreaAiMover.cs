0d96293488bf15b64f462baacd002f4a418ee298
lucasfagundesgweb <lucasfagundes.gweb@gmail.com>
2025-03-19 16:21:57 -0300

    feat: move count added to the area ai mover script.

--- Artists_Tavern_Project/Assets/Scripts/AI/AreaAiMover.cs
+++ Artists_Tavern_Project/Assets/Scripts/AI/AreaAiMover.cs
@@ -6,6 +6,9 @@ public class AreaAiMover : AiMover
     [SerializeField] float _duration = 1f;
     [SerializeField] int _areaIndex = 0;
     [SerializeField] Vector2 _waitTimeRange = default;
+    [SerializeField] bool _useMoveCount = false;
+    [SerializeField] int _moveCount = 0;
+    [SerializeField] int _maxMoveCount = 1;
 
     [Header("// Readonly")]
     [SerializeField] EnemySpawner _enemySpawner = null;
@@ -15,18 +18,19 @@ public class AreaAiMover : AiMover
     private void Start()
     {
         _enemySpawner = FindFirstObjectByType<EnemySpawner>();
+        ResetWaitTime();
         _timer = _waitTime;
-        ResetIdleTime();
     }
 
     private void Update()
     {
         _timer += Time.deltaTime;
 
-        if (_timer > _waitTime)
+        if (_timer > _waitTime && HasMovePoints())
         {
             _timer = 0f;
-            ResetIdleTime();
+            _moveCount++;
+            ResetWaitTime();
             Move();
         }
     }
@@ -37,8 +41,13 @@ public class AreaAiMover : AiMover
         _rb.DOMove(_position, _duration);
     }
 
-    public void ResetIdleTime()
+    public void ResetWaitTime()
     {
         _waitTime = Random.Range(_waitTimeRange.x, _waitTimeRange.y);
+    }
+
+    public bool HasMovePoints()
+    {
+        return !_useMoveCount || _useMoveCount && _moveCount < _maxMoveCount;
     }
 }
